ae2f322a5dcb1692d2ea7a847a67d0e35a4630f4
hossamnasser938 <hossamnasser938@gmail.com>
2023-09-28 21:29:29 +0300

    set cognito credentials

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "eu-north-1_Dv8jMXoGJ", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "j7tceb40sajp5olem3bfrmjv0", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-north-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
